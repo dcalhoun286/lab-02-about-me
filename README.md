f68216879f8f37d5075393d8ab8eca9866f77b0e
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2020-12-01 22:43:13 -0800

    added content to complete lab-02 assignment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# lab-02-about-me
+# LAB - 02
+
+## About Me
+
+I built a webpage to help people become more familiar with who I am. As a developer, for this project I wanted to create some fun prompts that engages the user regarding information contained in my page.
+
+### Feature Tasks
+* Create 5 prompts that require a Y/N answer regarding the content of the web page.
+* Ensure to include personalized messages so the user feels like they are directly interacting with the developer who created the page.
+
+### Author: Dar-Ci Calhoun
+
+### Links and Resources
+* [w3schools](https://www.w3schools.com)
+* [CSS Reset](https://meyerweb.com/eric/tools/css/reset/)
+
+### Reflections and Comments
+
+* It was so easy for me regarding this project to fall in the rabbit hole. It was a good lesson on prioritization of deliverables so that I accomplish the most crucial things in a timely manner.
+* My table mates admonished me to discontinue waiting so long to ACP my changes. This is going to be one of the things of utmost importance to improve on as I continue creating projects in the future.
+* W3 Schools really came through for me in figuring out many CSS rules that I either previously never learned, or needed another example to better conceptualize how to use that rule.
--- js/app.js
+++ js/app.js
@@ -1,43 +1,66 @@
 'use strict';
 
 var userName = prompt('What is your name?');
-console.log('The user name is ' + userName + '!');
+// console.log('The user name is ' + userName + '!');
+alert('Hello, ' + userName + '! Welcome.');
+alert('Let\'s do a fun quiz so you can get to know me better. Ready? Here we go ...');
 
 // Question One
 //DONE - ask question y/n or yes or no response - normalize case
-var homeTown = prompt('Do I live in Seattle?').toLowerCase();
+var homeTown = prompt('Question 1: Am I from Seattle?').toLowerCase();
 
 // I log my result to get proof of life!
 // console.log(homeTown);
 
-if (homeTown === 'yes' || homeTown === 'y'){
+if ((homeTown === 'no') || (homeTown === 'n')) {
   //DONE - console.log the answer IF IT IS CORRECT in a string concatenation
   //DONE - if it works, comment out console.log, send alert to user
   // console.log('Yes, I live in Seattle, ' + userName + ' ' + ' answered: ' + homeTown);
-  alert('That\'s Correct!');
+  // console.log(userName + ' is correct - not from Seattle.');
+  alert('That\'s Correct, ' + userName + '! I am not from Seattle.');
+} else if ((homeTown === 'yes') || (homeTown === 'y')) {
+  // console.log(userName + ' is incorrect - not from Seattle.');
+  alert('Wrong!');
 }
 
-// Question two
-//TODO - ask question
-//TODO - console.log the answer in a string concatenation
-//TODO - if it works, comment out console.log, send alert to user
+var onlyChild = prompt('Question 2: Am I an only child?').toLowerCase();
 
+if ((onlyChild === 'no') || (onlyChild === 'n')) {
+  // console.log(userName + ' is correct. I have siblings');
+  alert('That\'s correct, ' + userName + '! I have siblings.');
+} else if ((onlyChild === 'yes') || (onlyChild === 'y')) {
+  // console.log(userName + ' is incorrect. I am not an only onlyChild.');
+  alert('Wrong!');
+}
 
-// var booleanOne = true;  // 1 or 0
-// var booleanTwo = false;
-// var booleanThree = true;
+var manyHats = prompt('Question 3: Have I worn many hats throughout my job experiences?').toLowerCase();
 
-// if (booleanOne && booleanThree) {
-//   console.log('this worked');
-// }
-// //        true      false           true
-// if ((booleanOne || booleanTwo) && booleanThree) {
-//   console.log('will this work?');
-// }
-// if (booleanThree) {
-//   console.log('this should totally work!');
-// }
+if ((manyHats === 'yes') || (manyHats === 'y')) {
+  // console.log(userName + ' is correct. I\'ve worked many jobs');
+  alert('You got it, ' + userName + '! I have worked in quite a few industries over the years.');
+} else if ((manyHats === 'no') || (manyHats === 'n')) {
+  // console.log(userName + ' is incorrect. I\'ve worn many hats.');
+  alert('Wrong!');
+}
 
+var plantMom = prompt('Question 4: Do I like to take care of plants?').toLowerCase();
 
-// TODO change alert:  thank user, give meaning message with their name?
-// alert('Thanks for answering ' + userName + ', ' + homeTown + ' is great!');
+if ((plantMom === 'yes') || (plantMom === 'y')) {
+  // console.log(userName + ' is correct. I love plants.');
+  alert('Correct, ' + userName + '! I have tons of them!');
+} else if ((plantMom === 'no') || (plantMom === 'n')) {
+  // console.log(userName + ' is incorrect. I love plants.');
+  alert('Wrong!');
+}
+
+var healthCare = prompt('Last question: Am I or have I ever worked in healthcare?').toLowerCase();
+
+if ((healthCare === 'yes') || (healthCare === 'y')) {
+  // console.log(userName + ' is correct. I have been a healthcare worker.');
+  alert('That\'s right, ' + userName + '! I have healthcare experience.');
+} else if ((healthCare === 'no') || (healthCare === 'n')) {
+  // console.log(userName + ' is incorrect. I have healthcare experience.');
+  alert('Wrong!');
+}
+
+alert('Thanks for taking my About-Me quiz, ' + userName + '! I hope you enjoy looking at the rest of my web page.');
