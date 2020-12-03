4ef0e7aa52cafde8ff1f668af135486f37e09ce3
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2020-12-02 21:41:24 -0800

    added <ol> to index.html, updated Links and Resources in README.md, corrected broken links in js/app.js, added styling to lists in css/style.css

--- README.md
+++ README.md
@@ -13,9 +13,11 @@ I built a webpage to help people become more familiar with who I am. As a develo
 ### Links and Resources
 * [w3schools](https://www.w3schools.com)
 * [CSS Reset](https://meyerweb.com/eric/tools/css/reset/)
+* [Stack Overflow](https://stackoverflow.com/questions/34477280/comparing-user-input-to-values-from-an-array)
 
 ### Reflections and Comments
 
+**01 December 2020**
 * It was so easy for me regarding this project to fall in the rabbit hole. It was a good lesson on prioritization of deliverables so that I accomplish the most crucial things in a timely manner.
 * My table mates admonished me to discontinue waiting so long to ACP my changes. This is going to be one of the things of utmost importance to improve on as I continue creating projects in the future.
 * W3 Schools really came through for me in figuring out many CSS rules that I either previously never learned, or needed another example to better conceptualize how to use that rule.
--- js/app.js
+++ js/app.js
@@ -2,71 +2,71 @@
 
 // Greeting
 var userName = prompt('What is your name?');
-// // console.log('The user name is ' + userName + '!');
-// alert('Hello, ' + userName + '! Welcome.');
-// alert('Let\'s do a fun quiz so you can get to know me better. Ready? Here we go ...');
+// console.log('The user name is ' + userName + '!');
+alert('Hello, ' + userName + '! Welcome.');
+alert('Let\'s do a fun quiz so you can get to know me better. Ready? Here we go ...');
 
-// // Question One
-// //DONE - ask question y/n or yes or no response - normalize case
-// var homeTown = prompt('Question 1: Am I from Seattle?').toLowerCase();
+// Question One
+//DONE - ask question y/n or yes or no response - normalize case
+var homeTown = prompt('Question 1: Am I from Seattle?').toLowerCase();
 
-// // I log my result to get proof of life!
-// // console.log(homeTown);
+// I log my result to get proof of life!
+// console.log(homeTown);
 
-// if ((homeTown === 'no') || (homeTown === 'n')) {
-//   //DONE - console.log the answer IF IT IS CORRECT in a string concatenation
-//   //DONE - if it works, comment out console.log, send alert to user
-//   // console.log('Yes, I live in Seattle, ' + userName + ' ' + ' answered: ' + homeTown);
-//   // console.log(userName + ' is correct - not from Seattle.');
-//   alert('That\'s Correct, ' + userName + '! I am not from Seattle.');
-// } else {
-//   // console.log(userName + ' is incorrect - not from Seattle.');
-//   alert('Wrong!');
-// }
+if ((homeTown === 'no') || (homeTown === 'n')) {
+  //DONE - console.log the answer IF IT IS CORRECT in a string concatenation
+  //DONE - if it works, comment out console.log, send alert to user
+  // console.log('Yes, I live in Seattle, ' + userName + ' ' + ' answered: ' + homeTown);
+  // console.log(userName + ' is correct - not from Seattle.');
+  alert('That\'s Correct, ' + userName + '! I am not from Seattle.');
+} else {
+  // console.log(userName + ' is incorrect - not from Seattle.');
+  alert('Wrong!');
+}
 
-// // Question Two
-// var onlyChild = prompt('Question 2: Am I an only child?').toLowerCase();
+// Question Two
+var onlyChild = prompt('Question 2: Am I an only child?').toLowerCase();
 
-// if ((onlyChild === 'no') || (onlyChild === 'n')) {
-//   // console.log(userName + ' is correct. I have siblings');
-//   alert('That\'s correct, ' + userName + '! I have siblings.');
-// } else {
-//   // console.log(userName + ' is incorrect. I am not an only onlyChild.');
-//   alert('Wrong!');
-// }
+if ((onlyChild === 'no') || (onlyChild === 'n')) {
+  // console.log(userName + ' is correct. I have siblings');
+  alert('That\'s correct, ' + userName + '! I have siblings.');
+} else {
+  // console.log(userName + ' is incorrect. I am not an only onlyChild.');
+  alert('Wrong!');
+}
 
-// // Question Three
-// var manyHats = prompt('Question 3: Have I worn many hats throughout my job experiences?').toLowerCase();
+// Question Three
+var manyHats = prompt('Question 3: Have I worn many hats throughout my job experiences?').toLowerCase();
 
-// if ((manyHats === 'yes') || (manyHats === 'y')) {
-//   // console.log(userName + ' is correct. I\'ve worked many jobs');
-//   alert('You got it, ' + userName + '! I have worked in quite a few industries over the years.');
-// } else {
-//   // console.log(userName + ' is incorrect. I\'ve worn many hats.');
-//   alert('Wrong!');
-// }
+if ((manyHats === 'yes') || (manyHats === 'y')) {
+  // console.log(userName + ' is correct. I\'ve worked many jobs');
+  alert('You got it, ' + userName + '! I have worked in quite a few industries over the years.');
+} else {
+  // console.log(userName + ' is incorrect. I\'ve worn many hats.');
+  alert('Wrong!');
+}
 
-// // Question Four
-// var plantMom = prompt('Question 4: Do I like to take care of plants?').toLowerCase();
+// Question Four
+var plantMom = prompt('Question 4: Do I like to take care of plants?').toLowerCase();
 
-// if ((plantMom === 'yes') || (plantMom === 'y')) {
-//   // console.log(userName + ' is correct. I love plants.');
-//   alert('Correct, ' + userName + '! I have tons of them!');
-// } else {
-//   // console.log(userName + ' is incorrect. I love plants.');
-//   alert('Wrong!');
-// }
+if ((plantMom === 'yes') || (plantMom === 'y')) {
+  // console.log(userName + ' is correct. I love plants.');
+  alert('Correct, ' + userName + '! I have tons of them!');
+} else {
+  // console.log(userName + ' is incorrect. I love plants.');
+  alert('Wrong!');
+}
 
-// // Question 5
-// var healthCare = prompt('Question 5: Am I or have I ever worked in healthcare?').toLowerCase();
+// Question 5
+var healthCare = prompt('Question 5: Am I or have I ever worked in healthcare?').toLowerCase();
 
-// if ((healthCare === 'yes') || (healthCare === 'y')) {
-//   // console.log(userName + ' is correct. I have been a healthcare worker.');
-//   alert('That\'s right, ' + userName + '! I have healthcare experience.');
-// } else {
-//   // console.log(userName + ' is incorrect. I have healthcare experience.');
-//   alert('Wrong!');
-// }
+if ((healthCare === 'yes') || (healthCare === 'y')) {
+  // console.log(userName + ' is correct. I have been a healthcare worker.');
+  alert('That\'s right, ' + userName + '! I have healthcare experience.');
+} else {
+  // console.log(userName + ' is incorrect. I have healthcare experience.');
+  alert('Wrong!');
+}
 
 // Question 6
 // Guess my favorite number!
@@ -113,40 +113,52 @@ var foodGuessAnswer = foodGuessQuestion.toLowerCase();
 var yuckyFoods = ['anchovies', 'candy corn', 'olives', 'hawaiian pizza', 'pineapple pizza'];
 
 remainingGuesses = 6;
-youLose; // this might be unnecessary code as it has already been declared, but I will keep it here for now to better keep track as I try to form my while loop
+
 var found = false;
 
 for (var i = 0; i <= yuckyFoods.length; i++) {
 
-  if (foodGuessAnswer === yuckyFoods[i]) {
-
-    // when they guess a correct answer
-    console.log(`${userName} guessed correctly. ${foodGuessAnswer} is a food I dislike.`);
-    alert(`That's correct, ${userName}! `);
-
-    break;
-  }
-
-  console.log(`${userName} guessed wrong.`);
-
   remainingGuesses--;
 
-  if (remainingGuesses === 0) {
+  if (yuckyFoods[i] === foodGuessAnswer) {
 
-    alert(`Sorry, ${userName}, you are out of guesses! Here were the possible answers...`);
+    // when they guess a correct answer
+    found = true;
 
-    break;
+    console.log(`${userName} guessed correctly. ${foodGuessAnswer} is a food I dislike.`);
 
-  } else {
-    foodGuessQuestion = prompt(`Wrong! ${foodGuessAnswer} is not a correct answer. You have ${remainingGuesses} guess(es) left.`);
-    foodGuessAnswer = foodGuessQuestion.toLowerCase();
+    alert(`That's correct, ${userName}!`);
+  }
+
+  if (!found) {
+    for (var j = 1; j < yuckyFoods.length - 1; j++) {
+      if (yuckyFoods[j] === foodGuessAnswer) {
+        found = true;
+        alert(`That's correct, ${userName}!`);
+        break;
+      }
+    }
+    // if they guess incorrectly
+    if (remainingGuesses === 0) {
+
+      // user exhausted all of their guesses!
+      alert(`Sorry, ${userName}, you are out of guesses! Here were the possible answers...`);
+    } else if (foodGuessAnswer !== yuckyFoods[j]) {
+      foodGuessQuestion = prompt(`Wrong! ${foodGuessAnswer} is not a correct answer. You have ${remainingGuesses} guess(es) left.`);
+      foodGuessAnswer = foodGuessQuestion.toLowerCase();
+    }
   }
 }
 
+var listYuckyFoods;
+for (var k = 0; k < yuckyFoods.length; k++) {
+  if (i < yuckyFoods.length) {
+    listYuckyFoods += `${yuckyFoods[k]}` + ', ';
+  } else {
+    listYuckyFoods += `${yuckyFoods[k]}`;
+  }
+}
 
+alert('All possible correct answers: ' + listYuckyFoods);
 
-// user gets 6 guesses
-
-// the guesses end when player runs out of guesses OR if they guess a correct answer
-
-// alert('Thanks for taking my About-Me quiz, ' + userName + '! I hope you enjoy looking at the rest of my web page.');
+alert('Thanks for taking my About-Me quiz, ' + userName + '! I hope you enjoy looking at the rest of my web page.');
