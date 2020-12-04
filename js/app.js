f670701c45b1f05bc8999e0237ae79e864823b56
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2020-12-03 23:10:11 -0800

    changed nesting in questionSeven function to fix code that was failing to run

--- js/app.js
+++ js/app.js
@@ -2,26 +2,18 @@
 
 // Greeting
 var userName = prompt('What is your name?');
-// console.log('The user name is ' + userName + '!');
+
 alert('Hello, ' + userName + '! Welcome.');
+
 alert('Let\'s do a fun quiz so you can get to know me better. Ready? Here we go ...');
 
 function questionOne() {
   // Question One
-  //DONE - ask question y/n or yes or no response - normalize case
   var homeTown = prompt('Question 1: Am I from Seattle?').toLowerCase();
 
-  // I log my result to get proof of life!
-  // console.log(homeTown);
-
   if ((homeTown === 'no') || (homeTown === 'n')) {
-    //DONE - console.log the answer IF IT IS CORRECT in a string concatenation
-    //DONE - if it works, comment out console.log, send alert to user
-    // console.log('Yes, I live in Seattle, ' + userName + ' ' + ' answered: ' + homeTown);
-    // console.log(userName + ' is correct - not from Seattle.');
     alert('That\'s Correct, ' + userName + '! I am not from Seattle.');
   } else {
-    // console.log(userName + ' is incorrect - not from Seattle.');
     alert('Wrong!');
   }
 }
@@ -32,10 +24,8 @@ function questionTwo() {
   var onlyChild = prompt('Question 2: Am I an only child?').toLowerCase();
 
   if ((onlyChild === 'no') || (onlyChild === 'n')) {
-    // console.log(userName + ' is correct. I have siblings');
     alert('That\'s correct, ' + userName + '! I have siblings.');
   } else {
-    // console.log(userName + ' is incorrect. I am not an only onlyChild.');
     alert('Wrong!');
   }
 }
@@ -46,10 +36,8 @@ function questionThree() {
   var manyHats = prompt('Question 3: Have I worn many hats throughout my job experiences?').toLowerCase();
 
   if ((manyHats === 'yes') || (manyHats === 'y')) {
-    // console.log(userName + ' is correct. I\'ve worked many jobs');
     alert('You got it, ' + userName + '! I have worked in quite a few industries over the years.');
   } else {
-    // console.log(userName + ' is incorrect. I\'ve worn many hats.');
     alert('Wrong!');
   }
 }
@@ -60,10 +48,8 @@ function questionFour() {
   var plantMom = prompt('Question 4: Do I like to take care of plants?').toLowerCase();
 
   if ((plantMom === 'yes') || (plantMom === 'y')) {
-    // console.log(userName + ' is correct. I love plants.');
     alert('Correct, ' + userName + '! I have tons of them!');
   } else {
-    // console.log(userName + ' is incorrect. I love plants.');
     alert('Wrong!');
   }
 }
@@ -73,10 +59,8 @@ function questionFive() {
   // Question 5
   var healthCare = prompt('Question 5: Am I or have I ever worked in healthcare?').toLowerCase();
   if ((healthCare === 'yes') || (healthCare === 'y')) {
-    // console.log(userName + ' is correct. I have been a healthcare worker.');
     alert('That\'s right, ' + userName + '! I have healthcare experience.');
   } else {
-    // console.log(userName + ' is incorrect. I have healthcare experience.');
     alert('Wrong!');
   }
 }
@@ -88,18 +72,17 @@ function questionSix() {
   var faveNumber = parseInt(prompt(`Question 6: Now it\'s time to guess my favorite number, ${userName}! I am giving you 4 attempts to get it right.`));
 
   var youLose = false;
+
   var remainingGuesses = 4;
+
   var correctNumber = 5;
 
   while (!youLose) {
     if (faveNumber === correctNumber) {
-      // console.log(`${userName} guessed ${correctNumber}! That is right!`)
       alert(`That is correct, ${userName}! ${correctNumber} is my favorite number. Moving on to the next question...`);
 
       break;
     }
-
-    // console.log(`${userName} guessed wrong.`);
 
     remainingGuesses--;
 
@@ -121,16 +104,13 @@ questionSix();
 function questionSeven() {
   // Question 7
   // Guess the foods I think are gross!
-
   var foodGuessQuestion = prompt('Question 7: I have a list of some foods that I think are AWFUL. This list is definitely not exhaustive, but try to guess one!');
 
   var foodGuessAnswer = foodGuessQuestion.toLowerCase();
 
-  // TODO Store foods in an array - anchovies, candy corn, olives, Hawaiian pizza (also correct if they guess pineapple pizza)
-
   var yuckyFoods = ['anchovies', 'candy corn', 'olives', 'hawaiian pizza', 'pineapple pizza'];
 
-  remainingGuesses = 6;
+  var remainingGuesses = 6;
 
   var found = false;
 
@@ -139,12 +119,8 @@ function questionSeven() {
     remainingGuesses--;
 
     if (yuckyFoods[i] === foodGuessAnswer) {
-
       // when they guess a correct answer
       found = true;
-
-      console.log(`${userName} guessed correctly. ${foodGuessAnswer} is a food I dislike.`);
-
       alert(`That's correct, ${userName}!`);
     }
 
@@ -156,11 +132,21 @@ function questionSeven() {
           break;
         }
       }
+
       // if they guess incorrectly
       if (remainingGuesses === 0) {
-
         // user exhausted all of their guesses!
         alert(`Sorry, ${userName}, you are out of guesses! Here were the possible answers...`);
+        var listYuckyFoods;
+        for (var k = 0; k < yuckyFoods.length; k++) {
+          if (i < yuckyFoods.length) {
+            listYuckyFoods += `${yuckyFoods[k]}, `;
+          } else {
+            listYuckyFoods += `${yuckyFoods[k]}`;
+          }
+        }
+        alert('All possible correct answers: ' + listYuckyFoods);
+
       } else if (foodGuessAnswer !== yuckyFoods[j]) {
         foodGuessQuestion = prompt(`Wrong! ${foodGuessAnswer} is not a correct answer. You have ${remainingGuesses} guess(es) left.`);
         foodGuessAnswer = foodGuessQuestion.toLowerCase();
@@ -168,17 +154,7 @@ function questionSeven() {
     }
   }
 
-  var listYuckyFoods;
-  for (var k = 0; k < yuckyFoods.length; k++) {
-    if (i < yuckyFoods.length) {
-      listYuckyFoods += `${yuckyFoods[k]}` + ', ';
-    } else {
-      listYuckyFoods += `${yuckyFoods[k]}`;
-    }
-  }
 }
 questionSeven();
-
-alert('All possible correct answers: ' + listYuckyFoods);
 
 alert('Thanks for taking my About-Me quiz, ' + userName + '! I hope you enjoy looking at the rest of my web page.');
